c8a4af282f961d103bbd7738cfb4fc47e24e193b
gitirom <wadiaromdane10@gmail.com>
2026-01-19 16:01:04 +0100

    feat-Streamlit Web Page elements Creation: -create the st config and the side bar content

--- App.py
+++ App.py
@@ -26,11 +26,43 @@ nltk.download('stopwords')   #download stopwords for filltering out from resume 
 load_dotenv()  
 db_password = os.getenv('DB_PASSWORD')
 
-connection = pymysql.connect(  
-                                host='localhost',
-                                user='root',
-                                password='db_password',
-                                database='nlp_resume_app'
-                            )
+try:
+    connection = pymysql.connect(  
+        host='localhost',
+        user='root',
+        password= db_password,
+        database='nlp_resume_app'
+    )
 
-cursor = connection.cursor()
+    cursor = connection.cursor()
+    #streamlit notification for successful connection
+    st.success("Database connection successful!")
+except Exception as e:
+    st.error(f"Database connection failed: {e}")
+
+def insert_user(name,email,res_score,timestamp,no_of_pages,reco_field,cand_level,skills,recommended_skills,courses):
+    try:
+        DB_table_name = 'user_data'
+        insert_sql = "insert into " + DB_table_name + """ values (0,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+        rec_values = (name,email,res_score,timestamp,no_of_pages,reco_field,cand_level,skills,recommended_skills,courses)
+        cursor.execute(insert_sql, rec_values)
+        connection.commit()
+        st.success("User data inserted successfully!")
+    except Exception as e:
+        st.error(f"Failed to insert user data: {e}")
+
+# --- STREAMLIT WEB PAGE CONFIG ---
+st.set_page_config(
+    page_title="AI Resume Analyzer",
+    page_icon="./Logo/logo2.png",
+)
+
+def run():
+    img = Image.open("./Logo/logo2.png")
+    st.image(img,width=150)
+    st.title("AI Resume Analyzer and Job Recommender System")
+    st.sidebar.markdown("# Choose User Type")
+    activities = ["User", "Admin"]
+    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+
+    
