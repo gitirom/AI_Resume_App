3bf3412b690e48f86d3cd4146099adff84b3c3d7
gitirom <wadiaromdane10@gmail.com>
2026-01-19 14:57:07 +0100

    create the app and installed the required packages

---
+++ App.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import base64,random
+import time,datetime
+#libraries to parse the resume pdf files
+from pyresparser import ResumeParser
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+import io,random
+from streamlit_tags import st_tags
+from PIL import Image
+import pymysql
+from Courses import ds_course,web_course,android_course,ios_course,uiux_course,resume_videos,interview_videos
+import pafy #for uploading youtube videos
+import plotly.express as px #to create visualisations at the admin session
+import nltk
+
+nltk.download('stopwords')   #download stopwords for filltering out from resume content
+
