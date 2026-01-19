53326f1ec885c4bc4f1cb4261e23db2004d6018a
gitirom <wadiaromdane10@gmail.com>
2026-01-19 15:14:44 +0100

    feat-DBConnection: create the DB connection

--- App.py
+++ App.py
@@ -17,6 +17,20 @@ from Courses import ds_course,web_course,android_course,ios_course,uiux_course,r
 import pafy #for uploading youtube videos
 import plotly.express as px #to create visualisations at the admin session
 import nltk
+from dotenv import load_dotenv
+import os
 
 nltk.download('stopwords')   #download stopwords for filltering out from resume content
 
+# --- CONNECT TO DB ---
+load_dotenv()  
+db_password = os.getenv('DB_PASSWORD')
+
+connection = pymysql.connect(  
+                                host='localhost',
+                                user='root',
+                                password='db_password',
+                                database='nlp_resume_app'
+                            )
+
+cursor = connection.cursor()
